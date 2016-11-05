3ec96fa1f41deff331480b64f9ed69389d6b8191
lifeStine <vasus714@yandex.ru>
2016-11-05 08:58:32 +0300

    Fix ParseUrl

--- lab2/ParseUrl/ParseUrl/ParseUrl.cpp
+++ lab2/ParseUrl/ParseUrl/ParseUrl.cpp
@@ -8,12 +8,19 @@ int main()
 	int port;
 	std::string host;
 	std::string document;
-	while (std::getline(std::cin, url) && ParseURL(url, protocol, port, host, document))
+	while (std::getline(std::cin, url))
 	{
-		std::cout << url << std::endl;
-		std::cout << "HOST: " << host << std::endl;
-		std::cout << "PORT: " << port << std::endl;
-		std::cout << "DOC: " << document << std::endl;
+		if (ParseURL(url, protocol, port, host, document))
+		{
+			std::cout << url << std::endl;
+			std::cout << "HOST: " << host << std::endl;
+			std::cout << "PORT: " << port << std::endl;
+			std::cout << "DOC: " << document << std::endl;
+		}
+		else
+		{
+			std::cout << "Incorrect url" << std::endl;
+		}
 	}
     return 0;
 }
